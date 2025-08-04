2845483c7adee7c1d2bf740afb4a4a01dd079b47
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-08-03 22:53:13 -0600

    cloudflare -> netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
+import db from "@astrojs/db";
 
-import cloudflare from '@astrojs/cloudflare';
-
-
-import db from '@astrojs/db';
-
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap(), db()],
-  output: 'static',
-  adapter: cloudflare(),
+  output: "static",
+  adapter: netlify(),
 });
